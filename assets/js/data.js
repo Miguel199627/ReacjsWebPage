8224a5a6d78a612802e434c2a5727b5188d9c5bd
miguel199627 <cerquera199627@hotmail.com>
2021-07-08 15:04:56 -0500

    Updated code

---
+++ assets/js/data.js
@@ -0,0 +1,73 @@
+const data = [
+  {
+    name: "Death Note",
+    description:
+      "Light Yagami es un excelente estudiante japonés que ha tenido una vida aburrida. Esto cambia radicalmente cuando encuentra tirado en el suelo un cuaderno conocido como Death Note (Libreta de la Muerte), un cuaderno perdido por Ryuk, un Shinigami (Dios de la muerte).",
+    imgUrl: "assets/imgs/animes/deathNote.jpg",
+  },
+  {
+    name: "Dragon Ball Z",
+    description:
+      "En Dragon ball Z Goku se ha convertido en un adulto y está casado con Milk, con la que tiene un hijo llamado Gohan. En esta segunda saga de Dragon ball Goku descubrirá que no es un terricola, sino que pertenece a una raza de guerreros conocida por ser una de las más poderosas de la galaxia, para posteriormente dar paso a los verdaderos enemigos de la serie.",
+    imgUrl: "assets/imgs/animes/dragonBallZ.jpg",
+  },
+  {
+    name: "Nanatsu no Taizai: Fundo no Shinpan",
+    description: "Cuarta temporada de Nanatsu no Taizai",
+    imgUrl: "assets/imgs/animes/nanatsuNoTaizai.jpg",
+  },
+  {
+    name: "Fairy Tail",
+    description:
+      "Fairy Tail cuenta la historia de un joven mago llamado Natsu en la búsqueda de su maestro y padre adoptivo Igneel que resulta ser un dragón. Por ello se unió a Fairy Tail, un gremio de magos que es famoso por los destrozos y daños que provocan en diversas ciudades (lo cual no debería ser así).",
+    imgUrl: "assets/imgs/animes/fairyTail.jpg",
+  },
+  {
+    name: "Dr. Stone",
+    description:
+      "Senku es un joven extremadamente inteligente con un gran don para la ciencia y una ácida personalidad, y su mejor amigo es Taiju, que es muy buena persona pero más apto para usar los músculos que para pensar.",
+    imgUrl: "assets/imgs/animes/DrStone.webp",
+  },
+  {
+    name: "Vivy: Fluorite Eye's Song",
+    description:
+      "La historia tiene lugar en “Nearland”, un parque temático donde “los sueños, la esperanza y la ciencia” coexisten como un parque temático de Inteligencias Artificiales (IA). Este lugar es donde nace la primera IA automatizada de tipo humano.",
+    imgUrl: "assets/imgs/animes/vivy.jpg",
+  },
+  {
+    name: "Heroic Age",
+    description:
+      "Respondiendo al llamado de la 'Raza de Oro' (Gold Race), todas las razas se van al espacio. La raza humana es la ultima en salir, y por ello son conocidos como la 'Raza de Hierro'.Mucho tiempo ha pasado desde eso, y la humanidad es amenazada con la extinción a manos de la dominante 'Raza de Plata'. ",
+    imgUrl: "assets/imgs/animes/heroicAge.jpg",
+  },
+  {
+    name: "Tate no Yuusha no Nariagari",
+    description:
+      "Iwatani Naofumi es el típico otaku, al menos hasta que un día encuentra en la biblioteca un libro que lo transporta a otro mundo. Allí se convertirá en el Héroe del Escudo, uno de los Cuatro Héroes Cardinales, y tendrá que luchar contra las Olas de la Catástrofe junto a los héroes de la espada, la lanza y el arco.",
+    imgUrl: "assets/imgs/animes/tateNoYuushaNoNariagari.jpg",
+  },
+  {
+    name: "Kimetsu no Yaiba",
+    description:
+      "Estamos en la era Taisho de Japón. Tanjiro, un joven que se gana la vida vendiendo carbón, descubre un día que su familia ha sido asesinada por un demonio. Para empeorar las cosas, su hermana menor Nezuko, la única superviviente de la masacre, ha sufrido una transformación en demonio.",
+    imgUrl: "assets/imgs/animes/kimetsuNoYaiba.jpg",
+  },
+  {
+    name: "Ansatsu Kyoushitsu (TV)",
+    description:
+      "Los estudiantes de la clase 3-E tienen una misión: asesinar a su profesor antes de la graduación. Él ya ha destruido la luna, y prometió destruir la Tierra si no es asesinado dentro de un año. Pero, ¿cómo podrá esta clase de inadaptados asesinar a un pulpo extraterrestre, capaz de alcanzar la velocidad Mach 20, y que quizás ha sido el mejor profesor que han tenido?",
+    imgUrl: "assets/imgs/animes/ansatsuKyoushitsu.jpg",
+  },
+  {
+    name: "Hellsing",
+    description:
+      "La historia trata de un vampiro que no tiene nombre propio, pero que heredó de su familia el nombre de Alucard, el vampiro parece ser indestructible (no lo es), pero tiene un enemigo muy peligroso que tampoco tiene nombre, conocido como Incógnito, el objetivo de Alucard es destruir a Incógnito lo mas pronto posible, para no tener mas víctimas en su organización.",
+    imgUrl: "assets/imgs/animes/hellsing.jpg",
+  },
+  {
+    name: "Tengen Toppa Gurren Lagann",
+    description:
+      "La humanidad es obligada a vivir bajo la superficie pasando asi muchos siglos, llegando a olvidarse y convirtiendo en una leyenda la vida en la superficie. Asi se crean pequeñas colonias excavando por distintos lugares y tratando de escapar de los terribles terremotos. En una de esas colonias vive Simon, uno de los mejores excavadores, que en unos de los agujeros encuentra una extraña llave en forma de torno.",
+    imgUrl: "assets/imgs/animes/TengenToppaGurrenLagann.jpg",
+  },
+];
